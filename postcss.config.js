b764c24a574d3abe3d98ef1b5b83c2fb2aafb9f4
Himaja1729 <322103310064@gvpce.ac.in>
2025-07-20 22:09:32 +0530

    Fix build with correct PostCSS setup

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js
+
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // ✅ use this instead of 'tailwindcss'
+    autoprefixer: {},
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 // tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: 'class', // Enable class-based dark mode
     content: [
       "./app/**/*.{js,ts,jsx,tsx}",  // your Next.js app folder files
       "./components/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
       // add others if needed
     ],
     theme: {
